5b2f7b053df2f81f717eae708fea7cdf9614e7ce
Muhammad Emmad Siddiqui <72309760+MuhammadEmmadSiddiqui@users.noreply.github.com>
2021-10-21 12:52:07 +0500

    Create encoding image to base 64.py

---
+++ encoding image to base 64.py
@@ -0,0 +1,9 @@
+#importing base64 to convert png image to base64 format
+#base 64 is in built librarary in python
+import base64
+#opening image in (rerad binary by rb) and calling it imagefile 
+with open("Raspberry-Pi-GPIO-Header-with-Photo-702x336.png","rb") as imagefile:
+    byteform=base64.b64encode(imagefile.read())   #encoding it to byteform 
+f=open('output.bin','wb')  #opening a new binary file where we will store our image byteform we converted earlier
+f.write(byteform) #writing byteform in file name output.bin
+f.close()     #clsing it
